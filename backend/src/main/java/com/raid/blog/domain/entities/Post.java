be5356255fce7b03475f46e0d80f48a3e43109ef
Raid Thabet <thabet.raid123@gmail.com>
2025-06-18 11:13:54 +0100

    [backend] Added User Post relationship

--- backend/src/main/java/com/raid/blog/domain/entities/Post.java
+++ backend/src/main/java/com/raid/blog/domain/entities/Post.java
@@ -36,6 +36,10 @@ public class Post {
     @Column(nullable = false)
     private Integer readingTime;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
