0cc901dcd938fd9d78e5d19e1c6bef658f1462c1
Raid Thabet <thabet.raid123@gmail.com>
2025-06-18 13:41:00 +0100

    [backend] Added creating a new category

--- backend/src/main/java/com/raid/blog/controllers/CategoryController.java
+++ backend/src/main/java/com/raid/blog/controllers/CategoryController.java
@@ -1,19 +1,20 @@
 package com.raid.blog.controllers;
 
 import com.raid.blog.domain.dtos.CategoryDto;
+import com.raid.blog.domain.dtos.CreateCategoryRequest;
 import com.raid.blog.domain.entities.Category;
 import com.raid.blog.mappers.CategoryMapper;
 import com.raid.blog.services.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/categories")
+@RequestMapping("/api/v1/categories")
 @RequiredArgsConstructor
 class CategoryController {
 
@@ -28,5 +29,19 @@ class CategoryController {
                 .toList();
 
         return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<CategoryDto> createCategory(
+            @Valid @RequestBody CreateCategoryRequest createCategoryRequest
+    ) {
+        Category categoryToCreate = categoryMapper.toEntity(createCategoryRequest);
+
+        Category savedCategory = categoryService.createCategory(categoryToCreate);
+
+        return new ResponseEntity<>(
+                categoryMapper.toDto(savedCategory),
+                HttpStatus.CREATED
+        );
     }
 }
