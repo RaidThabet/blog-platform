828ca38f168169f6dc3f36a54ce37175958f9548
Raid Thabet <thabet.raid123@gmail.com>
2025-07-15 17:43:13 +0100

    Added validation for login request

--- backend/src/main/java/com/raid/blog/domain/dtos/LoginRequest.java
+++ backend/src/main/java/com/raid/blog/domain/dtos/LoginRequest.java
@@ -1,5 +1,9 @@
 package com.raid.blog.domain.dtos;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class LoginRequest {
+
+    @NotEmpty(message = "Email cannot be empty")
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email is invalid")
     private String email;
+
+    @NotEmpty(message = "Password cannot be empty")
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password should contain at least 8 characters")
     private String password;
 }
