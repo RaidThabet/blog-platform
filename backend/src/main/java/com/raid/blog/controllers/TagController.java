6b4331b6cf6235dac5d1c4668582b8e39ff6c5ad
Raid Thabet <thabet.raid123@gmail.com>
2025-07-16 14:35:22 +0100

    Added listing, creating, and deleting tags

---
+++ backend/src/main/java/com/raid/blog/controllers/TagController.java
@@ -0,0 +1,52 @@
+package com.raid.blog.controllers;
+
+import com.raid.blog.domain.dtos.CreateTagsRequest;
+import com.raid.blog.domain.dtos.TagResponse;
+import com.raid.blog.mappers.TagMapper;
+import com.raid.blog.services.TagService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/tags")
+@RequiredArgsConstructor
+public class TagController {
+
+    private final TagService tagService;
+    private final TagMapper tagMapper;
+
+    @GetMapping
+    public ResponseEntity<List<TagResponse>> getAllTags() {
+        List<TagResponse> tags = tagService.getTags().stream()
+                .map(tagMapper::toTagResponse)
+                .toList();
+
+        return ResponseEntity.ok(tags);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(
+            @RequestBody CreateTagsRequest request
+    ) {
+        var savedTags = tagService.createTags(request.getNames());
+        var createdTagResponse = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagResponse,
+                HttpStatus.CREATED
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTag(
+            @PathVariable UUID id
+    ) {
+        tagService.deleteTag(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
--- backend/src/main/java/com/raid/blog/repositories/TagRepository.java
+++ backend/src/main/java/com/raid/blog/repositories/TagRepository.java
@@ -2,8 +2,16 @@ package com.raid.blog.repositories;
 
 import com.raid.blog.domain.entities.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+    @Query("select t from Tag t left join fetch t.posts")
+    List<Tag> findAllWithPostCount();
+
+    List<Tag> findByNameIn(Collection<String> names);
 }
