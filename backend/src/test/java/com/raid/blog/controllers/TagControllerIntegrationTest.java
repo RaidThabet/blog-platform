967e065135d672a1d06c5f050ddd272ca708883f
Raid Thabet <thabet.raid123@gmail.com>
2025-08-07 15:41:54 +0100

    Added printing raw JSON requests in TagControllerIntegrationTest#createTags

--- backend/src/test/java/com/raid/blog/controllers/TagControllerIntegrationTest.java
+++ backend/src/test/java/com/raid/blog/controllers/TagControllerIntegrationTest.java
@@ -248,9 +248,13 @@ public class TagControllerIntegrationTest {
                     String.class
             );
 
+            String requestJSON = objectMapper.writeValueAsString(request);
+
             ObjectWriter writer = objectMapper.writerWithDefaultPrettyPrinter();
             String prettyJson = writer.writeValueAsString(objectMapper.readTree(response.getBody()));
+            String prettyJsonRequest = writer.writeValueAsString(objectMapper.readTree(requestJSON));
 
+            System.out.println("Raw JSON request: \n" + prettyJsonRequest);
             System.out.println("Raw JSON response: \n" + prettyJson);
             System.out.println("Response status: " + response.getStatusCode());
             System.out.println("Response headers: " + response.getHeaders());
